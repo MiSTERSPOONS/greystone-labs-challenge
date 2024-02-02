819e3d06a021d7f84b9fd4a799c1ac65a6ad89bc
MiSTERSPOONS <jonathanahn.js@gmail.com>
2024-02-02 17:25:20 -0500

    create src/db/initialize.py for all initialization

--- src/db/__init__.py
+++
@@ -1,7 +0,0 @@
-from sqlmodel import SQLModel, Session, create_engine
-
-engine = create_engine("sqlite:///database.db")
-session = Session(engine)
-
-def create_db_and_tables():
-    SQLModel.metadata.create_all(engine)

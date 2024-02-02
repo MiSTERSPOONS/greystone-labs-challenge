548bfc96f4af58c1da6e96dfd8cbb20196394b95
MiSTERSPOONS <jonathanahn.js@gmail.com>
2024-02-02 14:59:05 -0500

    SQLModel User model

---
+++ src/sqlmodel/models/user.py
@@ -0,0 +1,9 @@
+from typing import Optional
+from sqlmodel import Field, SQLModel
+
+class User(SQLModel, table=True):
+    user_id: Optional[int] = Field(default=None, primary_key=True)
+    first_name: str
+    last_name: str
+    email: str
+    password: str

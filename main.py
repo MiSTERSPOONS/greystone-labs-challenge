13d39c54d706ae1b2fc70bcd1b844f1a805cfa92
MiSTERSPOONS <jonathanahn.js@gmail.com>
2024-02-02 15:26:26 -0500

    main.py; add loan_router, user signup and create_db_and_tables func

--- main.py
+++ main.py
@@ -1,15 +1,23 @@
-from typing import Union
-
 from fastapi import FastAPI
+from src.sqlmodel.models.user import User
+from src.db import create_db_and_tables, session
+from src.routers.loans import loan_router
 
 app = FastAPI()
 
-
 @app.get("/")
 def read_root():
-    return {"Hello": "World"}
+    return "Welcome to the Greystone Loan Amortization App"
 
+app.include_router(loan_router)
 
-@app.get("/items/{item_id}")
-def read_item(item_id: int, q: Union[str, None] = None):
-    return {"item_id": item_id, "q": q}
+# User Routes
+@app.post("/signup", response_model=User)
+def signup(user: User):
+    session.add(user)
+    session.commit()
+    session.refresh(user)
+    return user
+
+if __name__ == "__main__":
+    create_db_and_tables()

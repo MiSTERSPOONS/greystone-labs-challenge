f2786d6a747976fc730a678a76a6c58b4ae94b55
MiSTERSPOONS <jonathanahn.js@gmail.com>
2024-02-03 12:06:54 -0500

    fetch all loans for a user

--- src/routers/loans.py
+++ src/routers/loans.py
@@ -1,5 +1,6 @@
-from fastapi import APIRouter
-from sqlmodel import Session
+from typing import Annotated, List
+from fastapi import APIRouter, Request, Query
+from sqlmodel import Session, select, col
 from src.db.initialize import engine
 from src.sqlmodel.models.loan import Loan
 
@@ -20,4 +21,12 @@ def create_loan(loan: Loan):
 # Fetch a loan schedule
 # Fetch a loan summary for a specific month
 # Fetch all loans for a user
+@loan_router.get("/all/", response_model=List[Loan])
+async def fetch_all_user_loans(
+    user_id: int
+):
+    with Session(engine) as session:
+        statement = select(Loan).where(col(Loan.user_id) == user_id)
+        results = session.exec(statement).all()
+        return results
 # Share loan with another user

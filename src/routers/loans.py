786ca6d2b47158a0852d0f8b9b97dfa9a593dfa7
MiSTERSPOONS <jonathanahn.js@gmail.com>
2024-02-03 18:57:16 -0500

    add a route to fetch a loann schedule

--- src/routers/loans.py
+++ src/routers/loans.py
@@ -3,6 +3,8 @@ from fastapi import APIRouter
 from sqlmodel import Session, select, col
 from src.db.initialize import engine
 from src.sqlmodel.models.loan import Loan
+from src.sqlmodel.models.loan_schedule import LoanSchedule
+from src.utils.loan_amortization_calculation import LoanAmortizationCalculator
 
 loan_router = APIRouter(
     prefix="/loans",
@@ -19,6 +21,23 @@ def create_loan(loan: Loan):
         return loan
 
 # Fetch a loan schedule
+@loan_router.get("/schedule/", response_model=List[LoanSchedule])
+async def fetch_loan_schedule(
+    loan_id: int
+):
+    with Session(engine) as session:
+        statement = select(Loan).where(col(Loan.loan_id) == loan_id)
+        results = session.exec(statement).all()
+        loan = results[0]
+
+        loan_schedule = LoanAmortizationCalculator.calculate_loan_schedule(
+            loan.loan_amount,
+            loan.loan_term_months,
+            loan.annual_interest_rate
+        )
+
+        return loan_schedule
+
 # Fetch a loan summary for a specific month
 # Fetch all loans for a user
 @loan_router.get("/all/", response_model=List[Loan])

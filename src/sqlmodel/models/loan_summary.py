760bcbbe59923ecc0535dca8b27a91713722b2e4
MiSTERSPOONS <jonathanahn.js@gmail.com>
2024-02-05 19:17:19 -0500

    update LoanSummary(SQLModel) class

--- src/sqlmodel/models/loan_summary.py
+++ src/sqlmodel/models/loan_summary.py
@@ -2,6 +2,6 @@ from decimal import Decimal
 from sqlmodel import SQLModel
 
 class LoanSummary(SQLModel):
-    principal_balance: Decimal
+    current_principal_balance: Decimal
     total_principal_paid: Decimal
     total_interest_paid: Decimal
